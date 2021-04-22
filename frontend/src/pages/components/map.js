2c0ada8a86ae4d4f318665c49ec5a33b4467616e
Shouyin <7000kl@163.com>
2021-04-22 12:23:00 +0800

    progress: map and hover

--- frontend/src/pages/components/map.js
+++ frontend/src/pages/components/map.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import ReactDOM from "react-dom";
 import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps";
 import geojsonAULess from "../shapes/geojsonAUless.json";
@@ -6,17 +6,15 @@ import geojsonAU from "../shapes/geojsonAU.json";
 import ReactMapGL, {Source, Layer} from 'react-map-gl';
 
 
-const projectionConfig = {
-  parallels: [-18, -36],
-  translate: [480, 250],
-  scale: 600,
-  rotate: [230, 0],
-  center: [5, 210]
-};
-
 const stroke = "#424642";
 const fill = "#F3F4ED";
 const choose = "#C06014";
+const mapkey = "pk.eyJ1Ijoic3N5aW4iLCJhIjoiY2tucmtvZWJqMDhjdTJvbWsydXEwdTRseiJ9.CLErMWYutp6PdHUaN5eFOg";
+const defaultViewport = {
+  latitude: - 25.27,
+  longitude: 133.77,
+  zoom: 3,
+};
 
 // https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/
 const layerStyle = {
@@ -29,77 +27,63 @@ const layerStyle = {
   }
 };
 
-function Map() {
-  const [viewport, setViewport] = useState({
-    latitude: -25.27,
-    longitude: 133.77,
-    zoom: 3
-  });
+function Map(props) {
+  let vp = defaultViewport;
+  let geojsonData = geojsonAU;
+  if (props.viewport) {
+    vp = props.viewport;
+  }
+  if (props.geojsonData) {
+    geojsonData = props.geojsonData;
+  }
+
+  const [viewport, setViewport] = useState(vp);
+  const [hoverInfo, setHoverInfo] = useState(null);
+
+  const onHover = useCallback(event => {
+    const {
+      features,
+      srcEvent: {x, y}
+    } = event;
+    const hoveredFeature = features && features[0];
+    // console.log(hoveredFeature)
+    setHoverInfo(
+      (hoveredFeature &&
+        hoveredFeature.properties &&
+        hoveredFeature.properties.feature_name) ? {
+        feature: hoveredFeature,
+        x: x,
+        y: y
+      }: null
+    );
+  }, []);
 
   return (
     <ReactMapGL
       {...viewport}
       width={"100%"}
       height={"100%"}
-      mapboxApiAccessToken={"pk.eyJ1Ijoic3N5aW4iLCJhIjoiY2tucmtvZWJqMDhjdTJvbWsydXEwdTRseiJ9.CLErMWYutp6PdHUaN5eFOg"}
+      mapboxApiAccessToken={mapkey}
       onViewportChange={nextViewport => setViewport(nextViewport)}
+      onHover={onHover}
     >
-      <Source id="my-data" type="geojson" data={geojsonAU}>
-        <Layer {...layerStyle} />
+      <Source id="mapdata" type="geojson" data={geojsonData}>
+        <Layer {...layerStyle}/>
       </Source>
+      {hoverInfo && (
+          <div className="tooltip" style={{position: "fixed", zIndex: 2, left: hoverInfo.x, top: hoverInfo.y}}>
+            <div>State: {hoverInfo.feature.properties.feature_name}</div>
+          </div>
+        )}
     </ReactMapGL>
   );
 }
 
-/*class GeoSvg extends React.Component {
-
-  state = {};
-
-  constructor(props) {
-    super();
-    this.allsvgs = props.allsvgs;
-    this.allsvgs[props.geo.rsmKey] = this;
-    this.state.geo = props.geo;
-    this.state.fill = fill;
-    this.state.stroke = stroke;
-  }
-
-  render() {
-    return <Geography
-      stroke={this.state.stroke}
-      fill={this.state.fill}
-      style={{
-        animationDuration: "1s",
-      }}
-      key={this.state.geo.rsmKey}
-      geography={this.state.geo}
-      onClick={() => {
-        for (let i of Object.keys(this.allsvgs)) {
-          this.allsvgs[i].setState({ fill: fill });
-        }
-        this.setState({ fill: choose })
-      }}
-      />;
-  }
-}*/
-
 let MainMap = (props) => {
+  // console.log(props.geojsonData);
   return <div style={{backgroundColor: "#424642", width: props.width, height: props.height} }>
-    <Map />
+    <Map viewport={props.viewport} geojsonData={props.geojsonData}/>
   </div>
-  
-
-  /* <ComposableMap projection={"geoConicEqualArea"} projectionConfig={projectionConfig}>
-      <ZoomableGroup zoom={1}>
-        <Geographies geography={geojsonAU}>
-          {({ geographies }) => {
-            let allsvgs = {}
-            return geographies.map(geo => <GeoSvg geo={geo} allsvgs={allsvgs} />)
-          }}
-        </Geographies>
-      </ZoomableGroup>
-    </ComposableMap>*/
-
 }
 
 export default MainMap;
--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,10 +1,22 @@
 import React from "react";
 import HomePageMap from "./components/map.js";
+import geojsonAU from "./shapes/geojsonAU.json";
+import geojsonAULess from "./shapes/geojsonAUless.json";
+import geojsonLGAvc from "./shapes/geojsonLGAvc.json";
 
 
 
 let HomePage = () => {
-  return <HomePageMap width={"100vw"} height={"100vh"} />
+  return <HomePageMap
+    width={"100vw"}
+    height={"100vh"}
+    viewport={{
+      latitude: - 25.27,
+      longitude: 133.77,
+      zoom: 3,
+    }} // initial viewport
+    geojsonData={geojsonLGAvc}
+  />
 }
 
 export default HomePage;
