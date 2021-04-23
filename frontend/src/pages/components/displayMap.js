c2a1916c45577f62706e468148ba29ecc8633f2c
Shouyin <7000kl@163.com>
2021-04-23 23:51:50 +0800

    progress: hp

--- frontend/src/pages/components/displayMap.js
+++ frontend/src/pages/components/displayMap.js
@@ -1,30 +1,35 @@
 import React, { useState, useCallback, useMemo } from "react";
 import ReactDOM from "react-dom";
-import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+} from "react-simple-maps";
 import geojsonAULess from "../shapes/geojsonAUless.json";
 import geojsonAU from "../shapes/geojsonAU.json";
-import ReactMapGL, {Source, Layer} from 'react-map-gl';
-
+import ReactMapGL, { Source, Layer } from "react-map-gl";
 
 const stroke = "#424642";
 const fill = "#F3F4ED";
 const choose = "#C06014";
-const mapkey = "pk.eyJ1Ijoic3N5aW4iLCJhIjoiY2tucmtvZWJqMDhjdTJvbWsydXEwdTRseiJ9.CLErMWYutp6PdHUaN5eFOg";
+const mapkey =
+  "pk.eyJ1Ijoic3N5aW4iLCJhIjoiY2tucmtvZWJqMDhjdTJvbWsydXEwdTRseiJ9.CLErMWYutp6PdHUaN5eFOg";
 const defaultViewport = {
-  latitude: - 25.27,
+  latitude: -25.27,
   longitude: 133.77,
   zoom: 3,
 };
 
 // https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/
 const layerStyle = {
-  id: 'au',
-  type: 'fill',
+  id: "au",
+  type: "fill",
   paint: {
     "fill-color": choose,
     "fill-opacity": 0.6,
     "fill-outline-color": stroke,
-  }
+  },
 };
 
 function Map(props) {
@@ -40,21 +45,23 @@ function Map(props) {
   const [viewport, setViewport] = useState(vp);
   const [hoverInfo, setHoverInfo] = useState(null);
 
-  const onHover = useCallback(event => {
+  const onHover = useCallback((event) => {
     const {
       features,
-      srcEvent: {x, y}
+      srcEvent: { x, y },
     } = event;
     const hoveredFeature = features && features[0];
     // console.log(hoveredFeature)
     setHoverInfo(
-      (hoveredFeature &&
+      hoveredFeature &&
         hoveredFeature.properties &&
-        hoveredFeature.properties.feature_name) ? {
-        feature: hoveredFeature,
-        x: x,
-        y: y
-      }: null
+        hoveredFeature.properties.feature_name
+        ? {
+            feature: hoveredFeature,
+            x: x,
+            y: y,
+          }
+        : null
     );
   }, []);
 
@@ -64,26 +71,42 @@ function Map(props) {
       width={"100%"}
       height={"100%"}
       mapboxApiAccessToken={mapkey}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
       onHover={onHover}
     >
       <Source id="mapdata" type="geojson" data={geojsonData}>
-        <Layer {...layerStyle}/>
+        <Layer {...layerStyle} />
       </Source>
       {hoverInfo && (
-          <div className="tooltip" style={{position: "fixed", zIndex: 2, left: hoverInfo.x, top: hoverInfo.y}}>
-            <div>State: {hoverInfo.feature.properties.feature_name}</div>
-          </div>
-        )}
+        <div
+          className="tooltip"
+          style={{
+            position: "fixed",
+            zIndex: 2,
+            left: hoverInfo.x,
+            top: hoverInfo.y,
+          }}
+        >
+          <div>State: {hoverInfo.feature.properties.feature_name}</div>
+        </div>
+      )}
     </ReactMapGL>
   );
 }
 
 let DisplayMap = (props) => {
   // console.log(props.geojsonData);
-  return <div style={{backgroundColor: "#424642", width: props.width, height: props.height} }>
-    <Map viewport={props.viewport} geojsonData={props.geojsonData}/>
-  </div>
-}
+  return (
+    <div
+      style={{
+        backgroundColor: "#424642",
+        width: props.width,
+        height: props.height,
+      }}
+    >
+      <Map viewport={props.viewport} geojsonData={props.geojsonData} />
+    </div>
+  );
+};
 
 export default DisplayMap;
--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,22 +1,116 @@
-import React from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import DisplayMap from "./components/displayMap.js";
 import geojsonAU from "./shapes/geojsonAU.json";
 import geojsonAULess from "./shapes/geojsonAUless.json";
 import geojsonLGAvc from "./shapes/geojsonLGAvc.json";
+import { headerStyle } from "./styles/homepageStyle";
+
+import { Fab, Paper, Tabs, Tab, Accordion, AccordionSummary, Typography, AccordionDetails, Button } from '@material-ui/core';
+import { Add as AddIcon, ExpandMore as ExpandMoreIcon } from '@material-ui/icons';
 
 
+const defaultViewport = {
+  latitude: -25.27,
+  longitude: 133.77,
+  zoom: 3,
+};
 
-let HomePage = () => {
+// mapType: different types of map
+let getMap = (data, mapType, geojson, viewport) => {
   return <DisplayMap
     width={"100vw"}
     height={"100vh"}
-    viewport={{
-      latitude: - 25.27,
-      longitude: 133.77,
-      zoom: 3,
-    }} // initial viewport
-    geojsonData={geojsonLGAvc}
+    viewport={viewport} // initial viewport
+    geojsonData={geojson}
   />
 }
+
+let getControlWrapper = (insideControl, title) => {
+  const [expanded, setExpanded] = useState(false);
+
+  return <Accordion expanded={expanded} onChange={(e, ex) => setExpanded(ex)}>
+    <AccordionSummary
+      expandIcon={<ExpandMoreIcon />}>
+      <Typography>{title}</Typography>
+    </AccordionSummary>
+    <AccordionDetails>
+      {insideControl}
+    </AccordionDetails>
+  </Accordion>
+}
+
+let getControl = (scenario, setGeoJsonData) => {
+
+  const [expanded, setExpanded] = useState(0);
+
+  // const handleChange = (p) => (e, expned) => setExpanded(expned? p:false);
+
+  // const handleChange = (p) => (e, expned) => setExpanded(expned ? p : false);
+  // const wrapper = (insideControl, title) => getControlWrapper(insideControl, title);
+
+  return <div style={{ position: "fixed", left: "32px", top: "128px", zIndex: 1, width: "20vw", minWidth: "280px" }}>
+    <Paper elevation={2} style={{margin: "0px 0px 16px 0px"}}>
+      <Typography variant="h6" component="h6" style={{padding: "16px 16px"}}>CONTROLS</Typography>
+    </Paper>
+    <div style={{ maxHeight: "calc(100vh - 240px)", overflowY: "scroll", overflowX: "visible"}}>
+      {getControlWrapper(<Button onClick={() => setGeoJsonData(geojsonAU)}>
+        change
+      </Button>, "Tweet index")}
+    
+      {getControlWrapper(<Typography>
+        Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+        maximus est, id dignissim quam.
+      </Typography>, "Complains about traffic")}
+    </div>
+  </div>
+}
+
+const scenario1 = 2;
+const scenario2 = 3;
+
+let HomePage = () => {
+
+  const [value, setValue] = useState(1);
+  const [geojsonData, setGeoJsonData] = useState(geojsonAULess)
+  const handleChange = (e, newv) => {
+    // value == 2: scenario 1
+    // value == 3: scenario 2
+    if (newv == scenario1) {
+      setGeoJsonData(geojsonLGAvc);
+    } else if (newv == scenario2) {
+      setGeoJsonData(geojsonAU);
+    } else {
+      setGeoJsonData(geojsonAULess);
+    }
+    setValue(newv);
+  }
+  
+  return (
+    <div>
+      <header style={headerStyle}>
+        <Paper elevation={3} square>
+          <Tabs
+            value={value}
+            indicatorColor="primary"
+            textColor="primary"
+            onChange={handleChange}
+            aria-label="header"
+          >
+            <Tab label="Assignment 2" disabled />
+            <Tab label="HOME" />
+            <Tab label="SCENARIO 1" />
+            <Tab label="SCENARIO 2" />
+          </Tabs>
+        </Paper>
+      </header>
+      {getControl("", setGeoJsonData)}
+      {getMap("", "", geojsonData, defaultViewport)}
+    </div>
+  );
+};
+
+/*<Fab color="primary" aria-label="add" style={{ position: "fixed", zIndex:1, bottom: "32px", left: "32px"}}>
+        <AddIcon />
+      </Fab> */
 
 export default HomePage;
