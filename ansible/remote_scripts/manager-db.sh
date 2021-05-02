95f43aa89949a2d45eb3ff6bd257088cee963a56
Shouyin <7000kl@163.com>
2021-05-02 23:53:41 +0800

    progress: create instance to couchdb cl

--- ansible/remote_scripts/manager-db.sh
+++ ansible/remote_scripts/manager-db.sh
@@ -4,4 +4,5 @@ sudo docker service create --replicas 3 --name couchdb --network couchdbnetwork 
   -e SERVICE_NAME="{{.Service.Name}}" \
   -e TASK_SLOT="{{.Task.Slot}}" \
   --publish published=5984,target=5984 \
+  --mount type=bind,src=/data/db,dst=/opt/couchdb/data \
   cclu
