9ea1f2726c0f314c8794a310d581a5c1af83e056
Shouyin <7000kl@163.com>
2021-05-05 12:46:51 +0800

    progress: commit and upl

---
+++ frontend/packfrontend.sh
@@ -0,0 +1,7 @@
+yarn build && zip -r build.zip build/
+
+scp build.zip ubuntu@172.26.129.77: && \
+scp build.zip ubuntu@172.26.133.93: && \
+scp build.zip ubuntu@172.26.130.32:
+
+ssh ubuntu@172.26.129.77 "unzip build.zip -d /data && mv -f /data/build /data/nginx"
