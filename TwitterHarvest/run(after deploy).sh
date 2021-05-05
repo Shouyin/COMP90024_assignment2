de507785736b26015aae8bce07f5725a7ffaf14d
Shouyin <7000kl@163.com>
2021-05-05 20:58:51 +0800

    progress: deploy to cluster

---
+++ TwitterHarvest/run(after deploy).sh
@@ -0,0 +1,26 @@
+if [[ -z "${TH_DB}" ]]; then
+  echo "no TH_DB (-db) specified"
+  exit 1
+fi
+
+if [[ -z "${TH_CITYNAME}" ]]; then
+  echo "no CITYNAME (-re) specified"
+  exit 1
+fi
+
+
+if [[ -z "${TH_KWCITYNAME}" ]]; then
+  echo "no CITYNAME (-kwreg) specified"
+  exit 1
+fi
+
+RUNSERVICE="sudo docker service create \
+    --name ${TH_CITYNAME} \
+    --network couchdbnetwork \
+    -e TH_DB=${TH_DB} \
+    -e TH_CITYNAME=${TH_CITYNAME} \
+    -e TH_KWCITYNAME=${TH_KWCITYNAME} \
+    th
+"
+
+ssh ubuntu@172.26.129.77 $RUNSERVICE
